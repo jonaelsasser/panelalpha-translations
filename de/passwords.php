23d5db6a6194e5b3d93a871e93760ff262d0e0a3
jonaelsasser <99296644+jonaelsasser@users.noreply.github.com>
2024-07-28 14:35:36 +0200

    New translations passwords.php (German)

--- de/passwords.php
+++ de/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
-    'sent' => 'Wir haben Ihren Link zum Zurücksetzen des Passworts per E-Mail gesendet!',
-    'throttled' => 'Bitte warten Sie, bevor Sie es erneut versuchen.',
-    'token' => 'Das Passwort Reset Token ist ungültig.',
-    'user' => "Wir konnten keinen Benutzer mit dieser E-Mail-Adresse finden.",
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
 
 ];
