33a796ae38321f1b940023ede10523a746a7e595
jonaelsasser <99296644+jonaelsasser@users.noreply.github.com>
2024-07-28 14:35:29 +0200

    New translations general.php (German)

--- de/api/general.php
+++ de/api/general.php
@@ -11,11 +11,11 @@ return [
         "uploading-database" => "Datenbank wird hochgeladen",
         'extracting-files'  => 'Dateien werden extrahiert',
         'extracting-database' => 'Datenbank wird extrahiert',
-        'importing-database' => 'Importiere Datenbank',
-        "configuring-wordpress" => "WordPress konfigurieren",
-        "cleaning-files" => "Dateien werden gereinigt",
-        "removing-ftp-account" => "FTP-Konto wird entfernt",
-        "creating-ftp-account" => "FTP-Konto erstellen",
-        "creating-temporary-directory" => "Temporäres Verzeichnis erstellen",
+        'importing-database' => 'Importing database',
+        "configuring-wordpress" => "Configuring WordPress",
+        "cleaning-files" => "Cleaning files",
+        "removing-ftp-account" => "Removing FTP account",
+        "creating-ftp-account" => "Creating FTP account",
+        "creating-temporary-directory" => "Creating temporary directory",
     ]
 ];
