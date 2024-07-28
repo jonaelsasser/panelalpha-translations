8c0c1c5eae2a105c9b8822448229ab325315e086
jonaelsasser <99296644+jonaelsasser@users.noreply.github.com>
2024-07-28 12:40:56 +0200

    New translations general.php (German)

---
+++ de/api/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    "import_steps" => [
+        "downloading-files" => "Downloading files",
+        "downloading-database" => "Downloading database",
+        "creating-hosting-account" => "Creating hosting account",
+        "creating-domain" => "Creating domain",
+        "creating-database" => "Creating database",
+        "uploading-files" => "Uploading files",
+        "uploading-database" => "Uploading database",
+        'extracting-files'  => 'Extracting files',
+        'extracting-database' => 'Extracting database',
+        'importing-database' => 'Importing database',
+        "configuring-wordpress" => "Configuring WordPress",
+        "cleaning-files" => "Cleaning files",
+        "removing-ftp-account" => "Removing FTP account",
+        "creating-ftp-account" => "Creating FTP account",
+        "creating-temporary-directory" => "Creating temporary directory",
+    ]
+];
